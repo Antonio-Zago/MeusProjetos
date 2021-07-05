84ca3e44eacf81f404d4c351d39b4d266108639d
AntonioZago <antoniozagodev@gmail.com>
2021-07-05 08:20:18 -0300

    mudanças nas aulas de c#

---
+++ Curso de C sharp/ExemploApi/ExemploApi/Controllers/ClientesController.cs
@@ -0,0 +1,21 @@
+﻿using ExemploApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ExemploApi.Controllers
+{
+    public class ClientesController : ApiController
+    {
+        private static List<Cliente> clientes= new List<Cliente>();
+
+        [HttpGet]
+        public List<Cliente> ListaClientes()
+        {
+            return clientes; 
+        }
+    }
+}
---
+++ Curso de C sharp/ExemploApi/ExemploApi/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExemploApi.Models
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+
+        //Construtor da classe 
+        public Cliente(string nome)
+        {
+            this.Nome = nome;
+        }
+    }
+}
