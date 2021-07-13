6b26cb8a202e6c23b7a7a552f2b0f77786618a88
Antonio-Zago <antoniozagodev@gmail.com>
2021-07-13 19:50:01 -0300

    Estudo Flutter

--- Curso de Flutter/login_tinder/lib/login/body.dart
+++ Curso de Flutter/login_tinder/lib/login/body.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:login_tinder/login/tema.dart';
 
 class body extends StatefulWidget {
-  const body({ Key? key }) : super(key: key);
+  const body({Key? key}) : super(key: key);
 
   @override
   _bodyState createState() => _bodyState();
@@ -14,22 +14,53 @@ class _bodyState extends State<body> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Stack(
-          children:[
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              color: Color(0xAAfe615d),
+        child: Stack(alignment: Alignment.center, children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            color: Color(0xAAfe615d),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 0, horizontal: MediaQuery.of(context).size.width * 0.2),
+            child: Column(
+              children: [
+                Container(
+                  child: Image.asset('assets/logo.png'),
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  height: MediaQuery.of(context).size.height * 0.4,
+                ),
+                Container(
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      "Location Changer",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height * 0.05,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height * 0.07,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(40)),
+                      child: RaisedButton(
+                    child: Text("Login Com Facebook", style: TextStyle(color: Colors.red),),
+                    onPressed: () {},
+                  )),
+                ),
+              ],
             ),
-            Container(
-              child: Image.asset('assets/logo.png'),
-            ),
-            Switch(value: Tema.mudanca.temaDark, onChanged: (valor){
-              Tema.mudanca.MudarTema();
-            }),
-          ]
-        ),
+          ),
+        ]),
       ),
-      );
+    );
   }
 }
